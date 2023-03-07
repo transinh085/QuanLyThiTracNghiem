fed87c723c913a9f8b8e795e8fdbc60a62f0b2a5
Trần Nhật Sinh <transinh085@gmail.com>
2023-03-07 17:20:13 +0700

    Revert "Update subject.js"
    This reverts commit dc5fc3c1876bf69f169102274df85df673e4f0f1.

--- public/js/pages/subject.js
+++ public/js/pages/subject.js
@@ -1,82 +1,29 @@
-Dashmix.onLoad(() =>
-    class {
-        static sweetAlert2() {
-            let e = Swal.mixin({
-                buttonsStyling: !1,
-                target: "#page-container",
-                customClass: {
-                    confirmButton: "btn btn-success m-1",
-                    cancelButton: "btn btn-danger m-1",
-                    input: "form-control",
-                },
-            });
-
-            document.querySelectorAll(".delete_roles").forEach((item) => {
-                item.addEventListener("click", (t) => {
-                    e.fire({
-                        title: "Are you sure?",
-                        text: "Bạn có chắc chắn muốn xoá nhóm quyền!",
-                        icon: "warning",
-                        showCancelButton: !0,
-                        customClass: {
-                            confirmButton: "btn btn-danger m-1",
-                            cancelButton: "btn btn-secondary m-1",
-                        },
-                        confirmButtonText: "Vâng, tôi chắc chắn!",
-                        html: !1,
-                        preConfirm: (e) =>
-                            new Promise((e) => {
-                                setTimeout(() => {
-                                    e();
-                                }, 50);
-                            }),
-                    }).then((t) => {
-                        t.value
-                            ? e.fire(
-                                "Deleted!",
-                                "Your imaginary file has been deleted.",
-                                "success"
-                            )
-                            : "cancel" === t.dismiss &&
-                            e.fire("Cancelled", "Your imaginary file is safe :)", "error");
-                    });
-                });
-            });
-        }
-        static init() {
-            this.sweetAlert2();
-        }
-    }.init()
-);
-
 function loadData() {
-    $.get(
-        "./subject/getData",
-        function (data, textStatus) {
+    $.get("./subject/getData",function (data) {
+            console.log(data);
             showData(data);
-        },
-        "json"
+        },"json"
     );
 }
 
 function showData(subjects) {
     let html = "";
     subjects.forEach((subject) => {
-        html += `<tr tid="${subject.mamonhoc}">
+        html += `<tr>
             <td class="text-center fs-sm"><strong>${subject.mamonhoc}</strong></td>
             <td>${subject.tenmonhoc}</td>
             <td class="d-none d-sm-table-cell text-center fs-sm">10</td>
             <td class="text-center">
-                <a class="btn btn-sm btn-alt-secondary" data-bs-toggle="modal" data-bs-target="#modal-block-vcenter" href="javascript:void(0)"
-                    data-bs-toggle="tooltip" aria-label="Thêm chương" data-bs-original-title="Thêm chương" action="add">
+                <a class="btn btn-sm btn-alt-secondary" href="javascript:void(0)"
+                    data-bs-toggle="tooltip" aria-label="Thêm chương" data-bs-original-title="Thêm chương">
                     <i class="fa fa-circle-info"></i>
                 </a>
-                <a class="btn btn-sm btn-alt-secondary" data-bs-toggle="modal" data-bs-target="#modal-edit-chapter" href="javascript:void(0)"
-                    data-bs-toggle="tooltip" aria-label="Sửa môn học" data-bs-original-title="Sửa môn học" action="update">
+                <a class="btn btn-sm btn-alt-secondary" href="javascript:void(0)"
+                    data-bs-toggle="tooltip" aria-label="Sửa môn học" data-bs-original-title="Sửa môn học">
                     <i class="fa fa-fw fa-pencil"></i>
                 </a>
-                <a class="btn btn-sm btn-alt-secondary delete_roles" href="javascript:void(0)"
-                    data-bs-toggle="tooltip" aria-label="Xoá môn học" data-bs-original-title="Xoá môn học" action="delete">
+                <a class="btn btn-sm btn-alt-secondary" href="javascript:void(0)"
+                    data-bs-toggle="tooltip" aria-label="Xoá môn học" data-bs-original-title="Xoá môn học">
                     <i class="fa fa-fw fa-times"></i>
                 </a>
             </td>
@@ -99,89 +46,11 @@ $("#add_class").on("click", function () {
         success: function (response) {
             if (response == true) {
                 $("#modal-add-subject").modal("hide");
-                loadData();
+                alert("Thêm thành công");
             } else {
             }
         },
     });
 });
-
-$(document).on("click", "tr td a[action='add']", function () {
-    var trid = $(this).closest("td").closest("tr").attr("tid");
-
-});
-
-$(document).on("click", "tr td a[action='update']", function () {
-    var trid = $(this).closest("td").closest("tr").attr("tid");
-    $("#mamon").val(trid);
-    $('#tenmon').val($(this).closest("td").closest("tr").children().eq(1).text())
-});
-
-$(document).on("click", "tr td a[action='delete']", function () {
-    var trid = $(this).closest("td").closest("tr").attr("tid");
-    let e = Swal.mixin({
-        buttonsStyling: !1,
-        target: "#page-container",
-        customClass: {
-            confirmButton: "btn btn-success m-1",
-            cancelButton: "btn btn-danger m-1",
-            input: "form-control"
-        }
-    });
-
-    e.fire({
-        title: "Are you sure?",
-        text: "Bạn có chắc chắn muốn xoá nhóm môn học?",
-        icon: "warning",
-        showCancelButton: !0,
-        customClass: {
-            confirmButton: "btn btn-danger m-1",
-            cancelButton: "btn btn-secondary m-1"
-        },
-        confirmButtonText: "Vâng, tôi chắc chắn!",
-        html: !1,
-        preConfirm: e => new Promise((e => {
-            setTimeout((() => {
-                e()
-            }), 50)
-        }))
-    }).then((t => {
-        if(t.value == true){
-            $.ajax({
-                type: "post",
-                url: "./subject/deleteData",
-                data: {
-                    mamon: trid
-                },
-                success: function (response) {
-                    e.fire("Deleted!", "Xóa môn học thành công!", "success")
-                    loadData();
-                }
-            });
-
-        } else {
-            e.fire("Cancelled", "Bạn đã không xóa môn học :)", "error")
-        }
-    
-    }))
-});
-
-$("#edit_class").click(function(e){
-
-
-    $.ajax({
-        type: "post",
-        url: "./subject/update",
-        data: {
-            mamon: $("#mamon").val(),
-            tenmon: $("#tenmon").val()
-        },
-        success: function (response) {
-            loadData();
-            $('#modal-edit-chapter').modal('hide');
-        }
-    });
-})
-
 
 loadData();

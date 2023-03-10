c33a36e9c7242f07076e5c924e0850d5a5271fd4
Trần Nhật Sinh <transinh085@gmail.com>
2023-03-11 00:10:40 +0700

    update roles

--- mvc/controllers/roles.php
+++ mvc/controllers/roles.php
@@ -1,5 +1,11 @@
 <?php
 class Roles extends Controller{
+    public $NhomQuyenModel;
+
+    public function __construct()
+    {
+        $this->NhomQuyenModel = $this->model("NhomQuyenModel");
+    }
 
     public function default()
     {
@@ -7,7 +13,8 @@ class Roles extends Controller{
             "Page" => "roles",
             "Title" => "Phân quyền",
             "Plugin" => [
-                "sweetalert2" => 1
+                "sweetalert2" => 1,
+                "notify" => 1
             ],
             "Script" => "roles"
         ]);
@@ -16,10 +23,25 @@ class Roles extends Controller{
     public function add()
     {
         if($_SERVER["REQUEST_METHOD"] == "POST") {
-            echo "<pre>";
-            print_r($_POST);
+            $name = $_POST['name'];
+            $roles = $_POST['roles'];
+            $result = $this->NhomQuyenModel->create($name,$roles);
+            echo $result;
+        }
+    }
+
+    public function getAllSl()
+    {
+        $result = $this->NhomQuyenModel->getAllSl();
+        echo json_encode($result);
+    }
+
+    public function getDetail()
+    {
+        if($_SERVER["REQUEST_METHOD"] == "POST") {
+            $result = $this->NhomQuyenModel->getById($_POST['manhomquyen']);
+            echo json_encode($result);
         }
     }
 }
-
 ?>

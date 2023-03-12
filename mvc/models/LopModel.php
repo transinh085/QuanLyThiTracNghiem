41f9ae1d703fdcb61354b48f871777d65c5a091d
Au Hao Nhien <115203499+nhienau@users.noreply.github.com>
2023-03-13 00:34:14 +0700

    Revert "update module"

--- mvc/models/LopModel.php
+++ mvc/models/LopModel.php
@@ -5,7 +5,7 @@ class LopModel extends DB{
     {
         // fix so 5, ma~ moi`
         $sql = "INSERT INTO `nhom`(`manhom`, `tennhom`, `mamoi`, `siso`, `ghichu`, `giangvien`, `manhomhp`) VALUES (NULL, '$name', '', 0, '$note', 5, '$group')";
-        return mysqli_query($this->con, $sql);
+        mysqli_query($this->con, $sql);
         // if($group != 0) {
         //     $id = mysqli_insert_id($this->con);
         //     $sql1 = "INSERT INTO `chitietnhom`(`manhom`, `malop`) VALUES ($group,$id)";
@@ -16,19 +16,6 @@ class LopModel extends DB{
     public function delete($id)
     {
         $sql = "DELETE FROM `nhom` WHERE manhom = $id";
-        return mysqli_query($this->con, $sql);
-    }
-
-    public function getInfo($id)
-    {
-        $sql = "SELECT * FROM `nhom` WHERE manhom = $id";
-        // return mysqli_query($this->con, $sql);
-        $result = mysqli_query($this->con, $sql);
-        return mysqli_fetch_assoc($result);
-    }
-
-    public function update($id, $name, $note) {
-        $sql = "UPDATE `nhom` SET `tennhom` = '$name', `ghichu` = '$note' WHERE `nhom`.`manhom` = $id";
         return mysqli_query($this->con, $sql);
     }
 

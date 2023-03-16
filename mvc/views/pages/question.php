c2db89a368a9a3b471659bf3a7c3264a27484891
hgbaodev <120194990+hgbaodev@users.noreply.github.com>
2023-03-17 02:04:14 +0700

    question

--- mvc/views/pages/question.php
+++ mvc/views/pages/question.php
@@ -328,3 +328,29 @@
         </div>
     </div>
 </div>
+
+<div class="modal fade" id="modal-add-subject" tabindex="-1" role="dialog" aria-labelledby="modal-add-subject"
+    aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered" role="document">
+        <div class="modal-content">
+            <div class="block block-rounded block-themed block-transparent mb-0">
+                <div class="block-header bg-primary-dark">
+                    <h3 class="block-title add-subject-element">Câu hỏi</h3>
+                    <div class="block-options">
+                        <button type="button" class="btn-block-option" data-bs-dismiss="modal" aria-label="Close">
+                            <i class="fa fa-fw fa-times"></i>
+                        </button>
+                    </div>
+                </div>
+                <div class="block-content fs-sm">
+
+                </div>
+                <div class="block-content block-content-full text-end bg-body">
+                    <button type="button" class="btn btn-sm btn-alt-secondary me-1"
+                        data-bs-dismiss="modal">Đóng</button>
+                    <button type="button" class="btn btn-sm btn-primary update-subject-element" id="update_subject" data-id="">Cập nhật</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>

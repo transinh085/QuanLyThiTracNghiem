0e8f220f9aaf95749d6c273e89a02e1e191d75cf
Trần Nhật Sinh <transinh085@gmail.com>
2023-03-17 00:44:53 +0700

    p

--- mvc/views/main_layout.php
+++ mvc/views/main_layout.php
@@ -1,10 +1,4 @@
 <?php require "inc/head.php" ?>
-<?php require_once "./mvc/models/NguoiDungModel.php"?>
-<?php
-if(!isset($_COOKIE['token'])){
-    echo "<script>window.location = './auth/signin'</script>";
-}
-?>
 <body>
     <!-- Page Container -->
     <div id="page-container" class="sidebar-o sidebar-light side-scroll page-header-fixed page-header-dark main-content-narrow remember-theme">

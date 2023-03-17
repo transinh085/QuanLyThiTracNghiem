ed36af746244a9685c7bebfefdd666fed678eab0
Trần Nhật Sinh <transinh085@gmail.com>
2023-03-17 13:45:48 +0700

    up

--- mvc/models/NhomModel.php
+++ mvc/models/NhomModel.php
@@ -1,41 +1,38 @@
 <?php
-class NhomModel extends DB {
-    // nhomhocphan
-    public function getAll()
+class NhomModel extends DB{
+    // nhom
+    public function create($name, $note, $group) 
     {
-        $sql = "SELECT * FROM `nhomhocphan`";
-        return mysqli_query($this->con, $sql);
-    }
-
-    public function insert($name) 
-    {
-        // $sql = "INSERT INTO `nhomhocphan`(`manhom`, `tennhom`, `mamoi`, `siso`, `ghichu`, `giangvien`, `manhomhp`) VALUES (NULL,'$name')";
-        $sql = "INSERT INTO `nhomhocphan`(`manhomhocphan`, `tennhomhocphan`, `khoa`) VALUES (NULL, '$name', 0)";
-        return mysqli_query($this->con, $sql);
-    }
-
-    public function update($id, $name)
-    {
-        $sql = "UPDATE `nhomhocphan` SET `tennhomhocphan` = '$name' WHERE `nhomhocphan`.`manhomhocphan` = $id";
-        return mysqli_query($this->con, $sql);
+        $valid = true;
+        $sql = "INSERT INTO `nhom`(`tennhom`,`ghichu`,`giangvien`, `manhomhp`) VALUES ('$name','$note', 1, $group)";
+        $result =  mysqli_query($this->con, $sql);
+        return $result;
+        // if(!$result) $valid = false;
+        // return $valid;
     }
 
     public function delete($id)
     {
-        $sql = "DELETE FROM `nhomhocphan` WHERE `nhomhocphan`.`manhomhocphan` = $id";
+        $valid = true;
+        $sql = "DELETE FROM `nhom` WHERE manhom = $id";
+        $result = mysqli_query($this->con, $sql);
+        if(!$result) $valid = false;
+        return $valid;
+    }
+
+    public function getInfo($id)
+    {
+        $sql = "SELECT * FROM `nhom` WHERE manhom = $id";
+        $result = mysqli_query($this->con, $sql);
+        return mysqli_fetch_assoc($result);
+    }
+
+    public function update($id, $name, $note) {
+        $sql = "UPDATE `nhom` SET `tennhom` = '$name', `ghichu` = '$note' WHERE `nhom`.`manhom` = $id";
         return mysqli_query($this->con, $sql);
     }
 
-    public function getClass($id)
-    {
-        $sql = "SELECT `nhom`.* FROM `nhom`, `nhomhocphan` WHERE `nhom`.`manhomhp` = `nhomhocphan`.`manhomhocphan` AND `nhom`.`manhomhp` = '$id'";
-        return mysqli_query($this->con, $sql);
-        // $result = mysqli_query($this->con, $sql);
-        // $rows = array();
-        // while($row = mysqli_fetch_assoc($result)) {
-        //     $rows[] = $row;
-        // }
-        // return $rows;
-    }
+    // chitietnhom
+
 }
 ?>

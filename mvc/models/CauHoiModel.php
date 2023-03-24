7791eb43242d96cdb79b7ba7d4227dd5c34789b9
hgbaodev <120194990+hgbaodev@users.noreply.github.com>
2023-03-25 01:02:26 +0700

    Update CauHoiModel.php

--- mvc/models/CauHoiModel.php
+++ mvc/models/CauHoiModel.php
@@ -27,7 +27,7 @@ class CauHoiModel extends DB{
 
     public function getAll()
     {
-        $sql = "SELECT * FROM cauhoi JOIN monhoc on  cauhoi.mamonhoc = monhoc.mamonhoc";
+        $sql = "SELECT * FROM cauhoi JOIN monhoc on  cauhoi.mamonhoc = monhoc.mamonhoc limit 5";
         $result = mysqli_query($this->con,$sql);
         $rows = array();
         while($row = mysqli_fetch_assoc($result)) {

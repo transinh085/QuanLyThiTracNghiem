dca5e2f90eee52ce4fbb30a45cfe474ee4ad3fa6
hgbaodev <120194990+hgbaodev@users.noreply.github.com>
2023-04-27 22:07:03 +0700

    updateUser

--- public/js/pages/dashboard.js
+++ public/js/pages/dashboard.js
@@ -3,13 +3,14 @@ $(document).ready(function(){
         type: "post",
         url: "./dashboard/checkEmail",
         success: function (response) {
-            console.log(response);
-            let i = document.getElementById("modal-onboarding");
-            new bootstrap.Modal(i).show(), 
-            i.addEventListener("shown.bs.modal", (i => {
-                document.querySelector("#modal-onboarding .js-slider").classList.remove("js-slider-enabled")
-                Dashmix.helpers("jq-slick") 
-            }))        
+            if(response == ""){
+                let i = document.getElementById("modal-onboarding");
+                new bootstrap.Modal(i).show(), 
+                i.addEventListener("shown.bs.modal", (i => {
+                    document.querySelector("#modal-onboarding .js-slider").classList.remove("js-slider-enabled")
+                    Dashmix.helpers("jq-slick") 
+                }))    
+            }
         }
     });
     
@@ -28,20 +29,41 @@ function checkEmailExist(email){
             email: email
         },
         success: function (response) {
-            console.log(response);
+            let check = response
+            if(check == 0){
+                updateEmail(email)
+            } else {
+                Dashmix.helpers('jq-notify', { type: 'danger', icon: 'fa fa-times me-1', message: `Email đã tồn tài trong hệ thống!` });
+            }
+        }
+    });
+}
+
+function updateEmail(email){
+    $.ajax({
+        type: "post",
+        url: "./dashboard/updateEmail",
+        data: {
+            email: email
+        },
+        success: function (response) {
+            if(response){
+                Dashmix.helpers('jq-notify', { type: 'success', icon: 'fa fa-times me-1', message: `Cập nhật email thành công bạn có thể đăng nhập bằng tài khoản google` });
+                $("#modal-onboarding").modal("hide");
+            } else {
+                Dashmix.helpers('jq-notify', { type: 'danger', icon: 'fa fa-times me-1', message: `Cập nhật email không thành công` });
+            }
         }
     });
 }
 
 $("#btn-email").click(function(){
     let email = $("#email").val();
-    if(validateEmail(email)){
-       let check =  checkEmailExist(email);
-       if(check == 1){
-        Dashmix.helpers('jq-notify', { type: 'danger', icon: 'fa fa-times me-1', message: `Vui lòng điền đúng định dạng email` });
-       } else {
-
-       }
+    if(email == ""){
+        Dashmix.helpers('jq-notify', { type: 'danger', icon: 'fa fa-times me-1', message: `Vui lòng không để trống email!` });
+    }
+    else if(validateEmail(email)){
+       checkEmailExist(email)
     } else {
         Dashmix.helpers('jq-notify', { type: 'danger', icon: 'fa fa-times me-1', message: `Vui lòng điền đúng định dạng email` });
     }

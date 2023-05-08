4d39c2669dfdc7f2c735c631b3fe80cacd3ff3ce
Trần Nhật Sinh <transinh085@gmail.com>
2023-05-08 11:56:40 +0700

    updatw

--- public/js/pages/signin.js
+++ public/js/pages/signin.js
@@ -1,9 +1,37 @@
-Dashmix.onLoad((()=>class{static initValidation(){Dashmix.helpers("jq-validation"),jQuery(".js-validation-signin").validate({rules:{"login-username":{required:!0,minlength:3},"login-password":{required:!0,minlength:5}},messages:{"login-username":{required:"Please enter a username",minlength:"Your username must consist of at least 3 characters"},"login-password":{required:"Please provide a password",minlength:"Your password must be at least 5 characters long"}}})}static init(){this.initValidation()}}.init()));
+Dashmix.onLoad((() => class {
+    static initValidation() {
+        Dashmix.helpers("jq-validation"), jQuery(".js-validation-signin").validate({
+            rules: {
+                "login-username": {
+                    required: !0,
+                    digits: true
+                },
+                "login-password": {
+                    required: !0,
+                    minlength: 5
+                }
+            },
+            messages: {
+                "login-username": {
+                    required: "Vui lòng nhập mã người dùng",
+                    digits: "Mã người dùng phải là số"
+                },
+                "login-password": {
+                    required: "Vui lòng nhập mã mật khẩu",
+                    minlength: "Mật khẩu phải lớn hơn 5 ký tự"
+                }
+            }
+        })
+    }
+    static init() {
+        this.initValidation()
+    }
+}.init()));
 
 
-$(".js-validation-signin").submit(function(e){
+$(".js-validation-signin").submit(function(e) {
     e.preventDefault();
-    if($(".js-validation-signin").valid()){
+    if ($(".js-validation-signin").valid()) {
         $.ajax({
             type: "POST",
             url: "./auth/checkLogin",
@@ -12,12 +40,16 @@ $(".js-validation-signin").submit(function(e){
                 password: $("#login-password").val(),
             },
             dataType: "json",
-            success: function (response) {
+            success: function(response) {
                 console.log(response.valid)
-                if(response.valid == 'true') {
+                if (response.valid == 'true') {
                     location.href = "./dashboard";
                 } else {
-                    Dashmix.helpers('jq-notify', { type: 'danger', icon: 'fa fa-times me-1', message: `${response.message}` });
+                    Dashmix.helpers('jq-notify', {
+                        type: 'danger',
+                        icon: 'fa fa-times me-1',
+                        message: `${response.message}`
+                    });
                 }
             }
         });
